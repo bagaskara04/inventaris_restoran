507314b1400aaa2126710f100c0af190a8217351
karalion9 <sagabdespair01@gmail.com>
2023-12-15 19:32:40 +0700

    perubahan

--- KelJobsheet10.java
+++ KelJobsheet10.java
@@ -23,45 +23,45 @@ public class KelJobsheet10 {
 
             switch (menu) {
                 case 1:
-                while (true) {
-                    System.out.print("Masukkan sisa makanan ringan: ");
-                    int sisa = sc.nextInt();
-                    System.out.print("Masukkan baris (0): ");
-                    int baris = sc.nextInt();
-                    System.out.print("Masukkan kolom (0/1/2): ");
-                    int kolom = sc.nextInt();
-                    sc.nextLine();
-                    makananRingan[baris][kolom] = sisa;
-                    
-                    System.out.print("Input sisa makanan ringan lainnya? (y/n): ");
-                    String next = sc.nextLine();
+                    while (true) {
+                        System.out.print("Masukkan sisa makanan ringan: ");
+                        int sisa = sc.nextInt();
+                        System.out.print("Masukkan baris (0): ");
+                        int baris = sc.nextInt();
+                        System.out.print("Masukkan kolom (0/1/2): ");
+                        int kolom = sc.nextInt();
+                        sc.nextLine();
+                        makananRingan[baris][kolom] = sisa;
 
-                    if (next.equalsIgnoreCase("n")) {
-                        break;
+                        System.out.print("Input sisa makanan ringan lainnya? (y/n): ");
+                        String next = sc.nextLine();
+
+                        if (next.equalsIgnoreCase("n")) {
+                            break;
+                        }
                     }
-                }
-                break;
+                    break;
 
                 case 2:
-                while (true) {
-                    System.out.print("Masukkan sisa minuman kaleng: ");
-                    int sisa = sc.nextInt();
-                    System.out.print("Masukkan baris (0): ");
-                    int baris = sc.nextInt();
-                    System.out.print("Masukkan kolom (0/1/2): ");
-                    int kolom = sc.nextInt();
-                    sc.nextLine();
-                    minumanKaleng[baris][kolom] = sisa;
-                    
-                    System.out.print("Input sisa minuman kaleng lainnya? (y/n): ");
-                    String next = sc.nextLine();
+                    while (true) {
+                        System.out.print("Masukkan sisa minuman kaleng: ");
+                        int sisa = sc.nextInt();
+                        System.out.print("Masukkan baris (0): ");
+                        int baris = sc.nextInt();
+                        System.out.print("Masukkan kolom (0/1/2): ");
+                        int kolom = sc.nextInt();
+                        sc.nextLine();
+                        minumanKaleng[baris][kolom] = sisa;
 
-                    if (next.equalsIgnoreCase("n")) {
-                    break;
+                        System.out.print("Input sisa minuman kaleng lainnya? (y/n): ");
+                        String next = sc.nextLine();
+
+                        if (next.equalsIgnoreCase("n")) {
+                            break;
+                        }
                     }
-                }
-                break;
-                
+                    break;
+
                 case 3:
                     System.out.println("Tampilkan sisa barang");
                     System.out.print("Makanan Ringan: ");
@@ -88,36 +88,35 @@ public class KelJobsheet10 {
                     }
                     System.out.println();
                     break;
-                
 
                 case 4:
-                while (true) {
-                    System.out.println("Tampilkan jumlah barang yang direstok");
-                    System.out.print("Makanan Ringan: ");
-                    for (int b = 0; b < makananRingan.length; b++) {
-                        for (int k = 0; k < makananRingan[b].length; k++) {
-                            int beli = total - makananRingan[b][k];
-                            if (beli > 0) {
-                                System.out.print(" " + beli + " ");
+                    while (true) {
+                        System.out.println("Tampilkan jumlah barang yang direstok");
+                        System.out.print("Makanan Ringan: ");
+                        for (int b = 0; b < makananRingan.length; b++) {
+                            for (int k = 0; k < makananRingan[b].length; k++) {
+                                int beli = total - makananRingan[b][k];
+                                if (beli > 0) {
+                                    System.out.print(" " + beli + " ");
+                                }
                             }
                         }
-                    } 
-                    System.out.println();
-                
-                    System.out.print("Minuman Kaleng: ");
-                    for (int b = 0; b < minumanKaleng.length; b++) {
-                        for (int k = 0; k < minumanKaleng[b].length; k++) {
-                            int beli = total - minumanKaleng[b][k];
-                            if (beli > 0) {
-                            System.out.print(" " + beli + " ");
+                        System.out.println();
+
+                        System.out.print("Minuman Kaleng: ");
+                        for (int b = 0; b < minumanKaleng.length; b++) {
+                            for (int k = 0; k < minumanKaleng[b].length; k++) {
+                                int beli = total - minumanKaleng[b][k];
+                                if (beli > 0) {
+                                    System.out.print(" " + beli + " ");
+                                }
                             }
                         }
+                        System.out.println();
+                        break;
                     }
-                    System.out.println();
                     break;
-                }
-                break;
-                
+
                 case 5:
                     System.out.println("Terima kasih");
                     System.exit(0);
